28d6a4099281b3b809310205ebaff73bf42df3e8
Evan Reaster <reaster@MacBook-Reaster.local>
2021-08-30 23:07:03 +0200

    fix the case 1 again

--- algo_even.c
+++ algo_even.c
@@ -6,7 +6,7 @@
 /*   By: earnaud <earnaud@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/02 17:52:59 by earnaud           #+#    #+#             */
-/*   Updated: 2021/08/30 23:02:19 by earnaud          ###   ########.fr       */
+/*   Updated: 2021/08/30 23:06:26 by earnaud          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,7 +54,7 @@ void	philo_sleep(t_philosopher *philo, long long sleep, int eat)
 
 void	eat_even(t_philosopher *philo)
 {
-	if (philo->param->nbr_philo % 2 && (get_time(philo->param) - philo->last_meal) + philo->param->time_to_eat > philo->param->time_to_die && (philo->param->time_to_eat * 2) + philo->param->time_to_sleep >= philo->param->time_to_die)
+	if (philo->param->nbr_philo != 1 && philo->param->nbr_philo % 2 && (get_time(philo->param) - philo->last_meal) + philo->param->time_to_eat > philo->param->time_to_die && (philo->param->time_to_eat * 2) + philo->param->time_to_sleep >= philo->param->time_to_die)
 	{
 	//	printf("fork too for %d time %lld last meal %lld time die %d\n",philo->id +1, get_time(philo->param), philo->last_meal, philo->param->time_to_die);
 		ft_sleep(philo->param->time_to_die - (get_time(philo->param) - philo->last_meal));
@@ -67,7 +67,7 @@ void	eat_even(t_philosopher *philo)
 		return;
 	}
 	write_action(TAKE_FORK, philo->id, philo->param, philo);
-	if (philo->param->nbr_philo % 2 && (get_time(philo->param) - philo->last_meal) + philo->param->time_to_eat > philo->param->time_to_die && (philo->param->time_to_eat * 2) + philo->param->time_to_sleep >= philo->param->time_to_die)
+	if (philo->param->nbr_philo != 1 && philo->param->nbr_philo % 2 && (get_time(philo->param) - philo->last_meal) + philo->param->time_to_eat > philo->param->time_to_die && (philo->param->time_to_eat * 2) + philo->param->time_to_sleep >= philo->param->time_to_die)
 	{
 	//	printf("fork too for %d time %lld last meal %lld time die %d\n",philo->id +1, get_time(philo->param), philo->last_meal, philo->param->time_to_die);
 		ft_sleep(philo->param->time_to_die - (get_time(philo->param) - philo->last_meal));
